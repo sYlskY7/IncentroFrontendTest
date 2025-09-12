5c4a29326b971e7e20cddb751b428961e42ebfd3
Pablo Parcha <pabloparcha7@gmail.com>
2025-09-12 19:26:28 +0200

    Added new books component

---
+++ incentro-cx-frontender-2025/src/app/features/books/pages/book-detail/book-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-detail',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      book-detail works!
+    </p>
+  `,
+  styles: ``
+})
+export class BookDetailComponent {
+
+}
